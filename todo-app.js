4674bc400d80effaf4a6fe0bdd83e3e9caee6093
shashannkd14 <shashannkd@msitprogram.net>
2020-05-11 00:22:33 +0530

    date validation added

--- todo-app.js
+++ todo-app.js
@@ -1,17 +1,16 @@
 let taskList = [];
 
 class Task {
-  constructor(name, dueDate, isDone) {
+  constructor(taskName, dueDate, isDone) {
     this.taskId = Date.now();
-    this.name = name;
+    this.taskName = taskName;
     this.dueDate = dueDate;
     this.isDone = isDone;
   }
 
   toString() {
     let htmlText = '<li class="task" ><div>';
-    htmlText += this.name;
-    htmlText += ", " + this.dueDate.getDate() + "/" + this.dueDate.getMonth();
+    htmlText += this.taskName + " " + this.dueDate;
     htmlText += '<input type="checkbox" name="isDone" id="isDone">';
     htmlText += '<button onclick="deleteTask(';
     htmlText += this.taskId;
@@ -24,7 +23,9 @@ class Task {
 function render() {
   const listUI = document.getElementById("todolist");
   listUI.innerHTML = "";
-  if (taskList.length === 0) listUI.innerHTML = "No tasks todo :-)";
+  // Get taskList from an API call
+
+  if (taskList.length === 0) listUI.innerHTML = "No tasks todo :)";
   taskList.forEach((task) => {
     listUI.innerHTML += task.toString();
   });
@@ -42,15 +43,40 @@ function deleteTask(taskId) {
 }
 
 function createTask() {
-  const taskName = document.getElementById("taskName").value;
-  addTask(new Task(taskName, new Date(), false));
+  let taskName = document.getElementById("taskName").value;
+  let dueDate = document.getElementById("dueDate").value;
+  if (validateDueDate(dueDate)) {
+    addTask(new Task(taskName, dueDate, false));
+  } else {
+    if (!document.body.contains(document.getElementById("errorMessage"))) {
+      let ele = document.createElement("h3");
+
+      ele.setAttribute("id", "errorMessage");
+      ele.innerHTML = "";
+      ele.innerHTML = "Please enter a valid due date.";
+      ele.style.color = "red";
+
+      document.getElementById("todolist").appendChild(ele);
+    }
+  }
 }
 
 function addTask(t) {
   taskList.push(t);
-  // call a web api to update the database on the server
+  // Call a web api to update the database on the server
   render();
   console.log(taskList);
+}
+
+function validateDueDate(dueDate) {
+  today = new Date();
+  parsedDueDate = new Date(
+    dueDate.split("-")[0],
+    dueDate.split("-")[1] - 1,
+    dueDate.split("-")[2]
+  );
+  if (parsedDueDate >= today) return true;
+  return false;
 }
 
 function init() {
@@ -62,9 +88,8 @@ function init() {
   // assign it to taskList
   // render
 
-  task = new Task("welcome task", new Date("May 30, 2020"), false);
-  addTask(task);
-  console.log(task);
+  render();
+  console.log(taskList);
 }
 
 init();
